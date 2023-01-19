d473751d688c7127fcd8fbb68a0ae5f2518fe1ac
Thinesan <thinesan@Thinesans-MBP-2.broadband>
2023-01-19 18:42:49 +0000

    first commit

---
+++ c8_coursework/week_1/day_4/Lab_ControlFlow/src/WhatToWear.java
@@ -0,0 +1,21 @@
+public class WhatToWear {
+    public static void main(String[] args) {
+
+        int temperature = 12;
+        String condition = "Sunny";
+
+
+        if (temperature <= 3 || condition == "Snow") {
+            System.out.println("Wear a winter insulated jacket!");
+        } else if ((temperature > 4 && temperature <= 15) && (condition == "Cloudy" || condition == "Rainy" || condition == "Sunny")) {
+            System.out.println("Wear a normal jacket!");
+        } else if ((temperature > 16 && temperature <= 25) && (condition == "Cloudy" || condition == "Rainy" || condition == "Sunny")) {
+            System.out.println("Wear a rain jacket");
+        } else if (temperature > 26 || (condition == "Cloudy" || condition == "Sunny" || condition == "Rainy")) {
+            System.out.println("No jacket needed !");
+        } else {
+            System.out.println("System Error");
+        }
+    }
+}
+
